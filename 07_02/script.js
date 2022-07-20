800bbf8b2e13b7dcd9d812b2cd6318e33b32232c
JoelHauser <joel.hauser@gmail.com>
2022-07-20 15:48:22 -0400

    updatE

--- 07_02/script.js
+++ 07_02/script.js
@@ -7,4 +7,19 @@ let item = "flashlight";
 
 const collection = ["Piggy", item, 5, true];
 
+collection[3] = "camera";
+collection[collection.length] = "new item";
+collection[9] = "bobble head";
+
 console.log(collection);
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+if (a == b) {
+  console.log("nope");
+} else {
+  console.log("yes");
+}
--- 07_04/script.js
+++ 07_04/script.js
@@ -2,10 +2,28 @@
  * Challenge: Build and modify an array
  * - Build an array with 8 items
  * - Remove the last item
- * - Add the last item as the first item on the array 
+ * - Add the last item as the first item on the array
  * - Sort the items by alphabetical order
  * - Use the find() method to find a specific item in the array
  * - Remove the item you found using the find method from the array.
  */
 
+const myArray = ["blanket", "computer", "ipad", "phone", "headphones"];
 
+console.log(myArray);
+
+myArray.pop();
+console.log("New Array:", myArray);
+
+myArray.unshift(myArray.pop());
+console.log("Last item now first:", myArray);
+
+myArray.sort();
+console.log("Sorted Array:", myArray);
+
+const foundItem = myArray.find((item) => item === "blanket");
+console.log(foundItem);
+
+let remove = "computer";
+myArray.splice(myArray.indexOf(remove), 1);
+console.log(`Array with "${remove}" removed:`, myArray);
